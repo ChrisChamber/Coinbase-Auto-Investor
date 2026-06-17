880c251fc1f35b89eb9aaaec7ce080c07eb8916d
ChrisChamber <you@example.com>
2026-06-17 22:24:27 +0100

    retrieving Eur and BTC wallets

--- go.mod
+++ go.mod
@@ -8,14 +8,7 @@ require (
 )
 
 require (
-	github.com/coinbase-samples/advanced-trade-sdk-go v0.4.0 // indirect
-	github.com/coinbase-samples/core-go v0.2.1 // indirect
-	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/shopspring/decimal v1.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.47.0 // indirect
-	golang.org/x/net v0.49.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
-	golang.org/x/text v0.33.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,46 +1,196 @@
 package main
 
 import (
-	"context"
+	"crypto/rand"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
-	"log"
+	"io"
+	"math"
+	"math/big"
+	"net/http"
 	"os"
+	"time"
 
-	"github.com/coinbase-samples/advanced-trade-sdk-go/accounts"
-	"github.com/coinbase-samples/advanced-trade-sdk-go/client"
-	"github.com/coinbase-samples/advanced-trade-sdk-go/credentials"
+	"github.com/joho/godotenv"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/square/go-jose/v3"
+	"github.com/square/go-jose/v3/jwt"
 )
 
+var (
+	keyName       = os.Getenv("COINBASE_KEY_NAME")
+	keySecret     = os.Getenv("COINBASE_KEY_SECRET")
+	requestMethod = "GET"
+	requestHost   = "api.coinbase.com"
+	requestPath   = "/api/v3/brokerage/accounts"
+)
+
+type APIKeyClaims struct {
+	*jwt.Claims
+	URI string `json:"uri"`
+}
+
+func buildJWT(uri string) (string, error) {
+	block, _ := pem.Decode([]byte(keySecret))
+	if block == nil {
+		return "", fmt.Errorf("jwt: Could not decode private key")
+	}
+
+	key, err := x509.ParseECPrivateKey(block.Bytes)
+	if err != nil {
+		return "", fmt.Errorf("jwt: %w", err)
+	}
+
+	sig, err := jose.NewSigner(
+		jose.SigningKey{Algorithm: jose.ES256, Key: key},
+		(&jose.SignerOptions{NonceSource: nonceSource{}}).WithType("JWT").WithHeader("kid", keyName),
+	)
+	if err != nil {
+		return "", fmt.Errorf("jwt: %w", err)
+	}
+
+	cl := &APIKeyClaims{
+		Claims: &jwt.Claims{
+			Subject:   keyName,
+			Issuer:    "cdp",
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Expiry:    jwt.NewNumericDate(time.Now().Add(2 * time.Minute)),
+		},
+		URI: uri,
+	}
+	jwtString, err := jwt.Signed(sig).Claims(cl).CompactSerialize()
+	if err != nil {
+		return "", fmt.Errorf("jwt: %w", err)
+	}
+	return jwtString, nil
+}
+
+var max = big.NewInt(math.MaxInt64)
+
+type nonceSource struct{}
+
+func (n nonceSource) Nonce() (string, error) {
+	r, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		return "", err
+	}
+	return r.String(), nil
+}
+
+type Account struct {
+	UUID             string `json:"uuid"`
+	Currency         string `json:"currency"`
+	Name             string `json:"name"`
+	AvailableBalance struct {
+		Currency string `json:"currency"`
+		Value    string `json:"value"`
+	} `json:"available_balance"`
+}
+
+type AccountsResp struct {
+	Accounts []Account `json:"accounts"`
+	Cursor   string    `json:"cursor"`
+	HasNext  bool      `json:"has_next"`
+	Size     int       `json:"size"`
+}
+
+func getJwt() string {
+	uri := fmt.Sprintf("%s %s%s", requestMethod, requestHost, requestPath)
+	jwt, err := buildJWT(uri)
+	if err != nil {
+		log.Errorf("error building jwt: %v", err)
+	}
+	return jwt
+}
+
+func getAccounts() (AccountsResp, error) {
+
+	req, _ := http.NewRequest("GET", "https://api.coinbase.com/api/v3/brokerage/accounts", nil)
+	req.Header.Set("Authorization", "Bearer "+getJwt())
+	req.Header.Set("Accept", "application/json")
+	resp, err := (&http.Client{}).Do(req)
+	if err != nil {
+		log.Errorf("error making request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("error reading response body: %v", err)
+	}
+
+	var accounts AccountsResp
+	if err := json.Unmarshal(body, &accounts); err != nil {
+		log.Fatalf("unmarshal accounts: %v", err)
+	}
+	var eur, btc *Account
+	// scan current page first
+	for i := range accounts.Accounts {
+		a := &accounts.Accounts[i]
+		if a.Currency == "EUR" && eur == nil {
+			eur = a
+		}
+		if a.Currency == "BTC" && btc == nil {
+			btc = a
+		}
+	}
+	// if either account is not found, keep fetching next pages until found or no more pages
+	for eur == nil || btc == nil {
+		if !accounts.HasNext {
+			break
+		}
+		req, _ := http.NewRequest("GET", "https://api.coinbase.com/api/v3/brokerage/accounts?cursor="+accounts.Cursor, nil)
+		req.Header.Set("Authorization", "Bearer "+getJwt())
+		req.Header.Set("Accept", "application/json")
+		resp, err := (&http.Client{}).Do(req)
+		if err != nil {
+			return accounts, fmt.Errorf("error making request: %w", err)
+		}
+		body, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			return accounts, fmt.Errorf("error reading response body: %w", err)
+		}
+		if err := json.Unmarshal(body, &accounts); err != nil {
+			return accounts, fmt.Errorf("unmarshal accounts: %w", err)
+		}
+
+		for i := range accounts.Accounts {
+			a := &accounts.Accounts[i]
+			if a.Currency == "EUR" && eur == nil {
+				eur = a
+			}
+			if a.Currency == "BTC" && btc == nil {
+				btc = a
+			}
+		}
+	}
+	if eur != nil {
+		fmt.Printf("EUR account: id=%s name=%s balance=%s %s\n", eur.UUID, eur.Name, eur.AvailableBalance.Value, eur.AvailableBalance.Currency)
+	} else {
+		fmt.Println("EUR account not found")
+
+	}
+	if btc != nil {
+		fmt.Printf("BTC account: id=%s name=%s balance=%s %s\n", btc.UUID, btc.Name, btc.AvailableBalance.Value, btc.AvailableBalance.Currency)
+	} else {
+		fmt.Println("BTC account not found")
+	}
+
+	return accounts, nil
+}
+
 func main() {
-	var creds credentials.Credentials
-
-	if err := json.Unmarshal([]byte(os.Getenv("ADVANCED_CREDENTIALS")), &creds); err != nil {
-		log.Fatalf("unable to deserialize advanced trade credentials JSON: %v", err)
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading .env file: %v", err)
 	}
 
-	httpClient, err := client.DefaultHttpClient()
+	accounts, err := getAccounts()
 	if err != nil {
-		log.Fatalf("unable to load default http client: %v", err)
+		log.Fatalf("error getting accounts: %v", err)
 	}
-
-	restClient := client.NewRestClient(&creds, httpClient)
-	//portfoliosService := portfolios.NewPortfoliosService(restClient)
-
-	accountsService := accounts.NewAccountsService(restClient)
-
-	resp, err := accountsService.ListAccounts(context.Background(), &accounts.ListAccountsRequest{})
-	if err != nil {
-		log.Fatalf("error listing accounts: %v", err)
-	}
-	//resp, err := portfoliosService.ListPortfolios(context.Background(), &portfolios.ListPortfoliosRequest{})
-	//if err != nil {
-	//	log.Fatalf("error listing portfolios: %v", err)
-	//}
-
-	jsonResponse, err := json.MarshalIndent(resp, "", "  ")
-	if err != nil {
-		panic(fmt.Sprintf("error marshaling response to JSON: %v", err))
-	}
-	fmt.Println(string(jsonResponse))
+	fmt.Printf("Retrieved %d accounts on final page\n", accounts.Size)
 }
