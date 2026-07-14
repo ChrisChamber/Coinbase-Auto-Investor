8cd03de1d79256dbf9f3e565e5a1b1bc2b5a7235
ChrisChamber <you@example.com>
2026-07-14 20:11:52 +0100

    saving orders to json file

--- main.go
+++ main.go
@@ -75,6 +75,10 @@ func main() {
 	}
 	discounts := []float64{0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.10}
 
+	storedOrders, err := loadStoredOrders()
+	if err != nil {
+		log.Fatalf("error loading stored orders: %v", err)
+	}
 	for i, discount := range discounts {
 		// putting in 10 orders at a time, each with the same size, but the buy price will decrease by a percentage with each order
 		limitPrice := currentBuyPrice * (1 - discount)
@@ -100,16 +104,18 @@ func main() {
 		}
 		fmt.Printf("Order created: %+v\n", order.SuccessResponse.OrderID)
 
-		// Store the order
-		stored := StoredOrder{
+		// accumulate the order details in the storedOrders slice to save them later
+		storedOrders = append(storedOrders, StoredOrder{
 			OrderID:       order.SuccessResponse.OrderID,
 			ClientOrderID: order.SuccessResponse.ClientOrderID,
 			ProductID:     order.SuccessResponse.ProductID,
 			Side:          order.SuccessResponse.Side,
 			Status:        "OPEN",
-		}
+		})
 
 	}
-	saveStoredOrders([]StoredOrder{stored})
+	if err := saveStoredOrders(storedOrders); err != nil {
+		log.Fatalf("error saving stored orders: %v", err)
+	}
 
 }
